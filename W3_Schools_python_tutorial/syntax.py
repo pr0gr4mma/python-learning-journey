79a0a1719439004bd93dece22f5cb1b565709ad5
pr0gr4mma <pr0gr4mma@gmail.com>
2025-12-21 09:37:45 +1100

    Add w3 schools python syntax tutorial.

---
+++ W3_Schools_python_tutorial/syntax.py
@@ -0,0 +1,29 @@
+# comments always start with (#) in python.
+"""
+This is a comment
+written in
+more than just one line
+"""
+# Executing Python Syntax
+# Python can be executed bu writing directly in the command line or by creating a python file on a server (.py) and running it in the command line.
+#e.g. C:\dev\file.py
+
+# Indentation in Python is very important, other programming languages only use it for readibility but in python it serves a purpose.
+
+if 5 > 2:
+    print("Five is greater than two.")
+
+# Python renders the above indentation as a block of code.
+
+if 5 > 2:
+print("Five is greater than two.")
+
+# Python will give you an error if you skip indentation like above.
+
+# Semicolons are very rare but optional in Python. You can write multiple statements on one line by seperating them with ;
+
+print("Hello"); print("Bye!")
+
+# However if you put two statements on one line without a seperator (;) Python will give this an error.
+
+print("Hello"); print("Bye!")
