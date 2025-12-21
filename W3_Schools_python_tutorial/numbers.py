9a369448fbc50a6151a72ee01e1d007064f05eda
pr0gr4mma <pr0gr4mma@gmail.com>
2025-12-21 15:47:42 +1100

    Add numbers python tutorial

---
+++ W3_Schools_python_tutorial/numbers.py
@@ -0,0 +1,16 @@
+# Print Numbers
+# You can also use print() function to display numbers.
+# However, numbers are not put inside quotes.
+
+print(3)
+print(10)
+print(5000)
+
+# You can also do Math inside a print() function.
+
+print(4 + 6)
+print(5 * 8)
+
+# You are also able to mix text and number
+
+print("I am", 30, "next year.")
