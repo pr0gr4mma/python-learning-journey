73dca87d2d8618bc938936c193b010181630629d
pr0gr4mma <pr0gr4mma@gmail.com>
2025-12-21 15:13:05 +1100

    Add output python tutorial to github

---
+++ W3_Schools_python_tutorial/output.py
@@ -0,0 +1,20 @@
+# Print text
+# To print text use the print() function.
+
+print("Hello World!")
+
+# You can use print as many times as you want. Each print() call displays on a new line.
+
+print("Hey,")
+print("How are you?")
+
+# You can also use ëither "" or '' quotes.
+print('This will work')
+print("This will also work")
+
+# If you do not put the text inside print within quotes it will give an error.
+print(hi)
+
+# If you would like to print multiple words on the same line you can use the end parameter.
+print("G'day mate,", end=" ")
+print("How are you?")
