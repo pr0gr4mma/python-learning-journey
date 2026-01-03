4d97a6e551751c98080a4778a3ff5e4c9cb83e51
pr0gr4mma <pr0gr4mma@gmail.com>
2026-01-03 22:50:49 +1100

    Add Integers and floats to numbers.py

--- W3_Schools_python_tutorial/numbers.py
+++ W3_Schools_python_tutorial/numbers.py
@@ -7,6 +7,8 @@ a = 1 # int
 b = 3.4 # float
 c = 1j
 
+# To verify the type of onbject in python use the type() function
+
 print(type(a))
 print(type(b))
 print(type(c))
@@ -28,3 +30,33 @@ print(5 * 8)
 # You are also able to mix text and number
 
 print("I am", 30, "next year.")
+
+# Integers is a whole number positive or negative number without decimals of unlimited length.
+
+d = 1
+e = 2384734
+f = -204332
+
+print(type(d))
+print(type(e))
+print(type(f))
+
+# Float numbers are a positive or negative number with one or more decimals
+
+g = 1.3
+h = 1232.24124321
+i = -24432982.23412
+
+print(type(g))
+print(type(h))
+print(type(i))
+
+# Float can also be scientific numbers with an "e" to indicate the power of 10.
+
+j = 35e3
+k = 2349E23
+l = -23e4
+
+print(type(j))
+print(type(k))
+print(type(l))
