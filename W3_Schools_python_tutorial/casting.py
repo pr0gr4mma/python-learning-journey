f3d8ca81280f7037d943e54fd63c1dba2eb16e95
pr0gr4mma <pr0gr4mma@gmail.com>
2026-01-11 21:48:55 +1100

    W3 Schools python casting tutorial

---
+++ W3_Schools_python_tutorial/casting.py
@@ -0,0 +1,39 @@
+# Python Casting
+# Specify a variable type
+# There may be times when you want to specify a type on to a variable. This can be done with casting.
+# Python is an object orientated language, and as such it uses classes to define data types, including its primitive types.
+# Casting in python is therefore done using constructor functions:
+# int()
+# float()
+# str()
+
+# Integer example
+a = int(1)
+b = int(2.8)
+c = int("3")
+
+print(a)
+print(b)
+print(c)
+
+# Floats
+
+a = float(1)
+b = float(2.8)
+c = float("3")
+f = float("4.2")
+
+print(a)
+print(b)
+print(c)
+print(f)
+
+a = str(1)
+b = str(2.8)
+c = str("3")
+f = str("4.2")
+
+print(a)
+print(b)
+print(c)
+print(f)
