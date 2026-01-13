dd658dc532accf18884de61ab71eb951372232fd
pr0gr4mma <pr0gr4mma@gmail.com>
2026-01-13 21:29:35 +1100

    Adding w3 strings tutorial and how to write them

---
+++ W3_Schools_python_tutorial/strings.py
@@ -0,0 +1,28 @@
+# Strings
+# strings in python are surrounded by either single '' or double quotes "" in python and they output the same.
+print("Hello")
+print('Hello')
+
+# You can use quotes inside a string as long as they don't match the outside quotes of the string.
+
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+# You can assign a string to a variable.
+
+a = "G'day"
+print(a)
+
+# Multi line strings can be done by using three quotation marks around each side of the string """ or '''
+
+b = """"This is a,
+string that I have created
+it's multiline."""
+
+c = '''This is a,
+string that I have created
+it's multiline.'''
+
+print(b)
+print(c)
